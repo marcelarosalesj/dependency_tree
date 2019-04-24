4f046ea9847ffb87c6d9c8d80767888aaeb27cd3
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-24 16:52:34 -0500

    Changing parsing spec from manually to pyrpm tool
    - pyrpm tool seems to be a little bit abandoned, but for now it's
    ok. Later I'll decide if keep using it or going for a different one.
    - A current issue with the stx graph generation is that many nodes
    are generated for the same deb package. This happens because in this
    script a node is generated for every BuildRequires.
    The very next problem to fix is change the code to use an existing
    node instead of creating a new one. And then doing the add_edge on
    that existing node.
    
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- stx_dependency_tree.py
+++ stx_dependency_tree.py
@@ -2,25 +2,36 @@ import glob
 import networkx as nx
 import re
 import os
+from pyrpm.spec import Spec # Other options?
+import ipdb
 
 files = glob.glob('./**/*.spec', recursive=True)
 
 G = nx.DiGraph()
+count=0
+for ff in files:
+    with open(ff) as spec:
+        # Create RPM Spec Object
+        ss = Spec.from_file(ff)
+        # Get from path useful information
+        packagespec = ff.split('/')[-1].split('.')[0] # get name of spec...
+        attr_path = ff
+        attr_project = ff.split('/stx/')[-1].split('/')[0] # get name of project
+        # Parse each of the packages in a spec file
+        for pkg in ss.packages:
+            G.add_node(pkg.name, path=attr_path, project=attr_project)
+            for br in pkg.build_requires:
+                G.add_node(br, path=attr_path, project=attr_project)
+                G.add_edge(pkg.name, br)
+            if ss.name == pkg.name:
+                count = count +1
+                for br in ss.build_requires:
+                    G.add_node(br, path=attr_path, project=attr_project)
+                    G.add_edge(ss.name, br)
 
-for f in files:
-    with open(f) as spec:
-        packagespec = f.split('/')[-1].split('.')[0]
-        attr_path = f
-        attr_project = f.split('/stx/')[-1].split('/')[0]
-        G.add_node(packagespec, path=attr_path, project=attr_project)
-        for line in spec.readlines():
-            r1 = re.match(r"^BuildRequires:", line)
-            if r1 is not None:
-                buildreq = line.split('BuildRequires:')[-1].strip()
-                G.add_edge(packagespec, buildreq)
+
 
 pos=nx.spring_layout(G)
-
 # Generating some results
 try:
     os.mkdir('results')
@@ -29,5 +40,7 @@ except FileExistsError as e:
 nx.write_graphml_xml(G, "results/xml")
 nx.write_adjlist(G, 'results/adjlist')
 
+
+print(count)
 
 
