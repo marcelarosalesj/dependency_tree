a663769f0d8d4c03e8ee9f53ebb69774698aa783
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-24 12:57:35 -0500

    Modify stx_dependency_tree.py script to use networkx
    - it is saved in xml and adjlist format
    - also fix some errors when manipulating strings from spec files
    
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- stx_dependency_tree.py
+++ stx_dependency_tree.py
@@ -1,17 +1,33 @@
-
 import glob
+import networkx as nx
+import re
+import os
 
 files = glob.glob('./**/*.spec', recursive=True)
 
-graph = {}
+G = nx.DiGraph()
 
 for f in files:
     with open(f) as spec:
         packagespec = f.split('/')[-1].split('.')[0]
-        graph[packagespec] = []
-        #import pdb; pdb.set_trace()
+        attr_path = f
+        attr_project = f.split('/stx/')[-1].split('/')[0]
+        G.add_node(packagespec, path=attr_path, project=attr_project)
         for line in spec.readlines():
-            if 'BuildRequires' in line:
-                graph[packagespec].append(line.split(':')[-1].strip())
+            r1 = re.match(r"^BuildRequires:", line)
+            if r1 is not None:
+                buildreq = line.split('BuildRequires:')[-1].strip()
+                G.add_edge(packagespec, buildreq)
 
-print(graph)
+pos=nx.spring_layout(G)
+
+# Generating some results
+try:
+    os.mkdir('results')
+except FileExistsError as e:
+    print('W: results directory already exists. Files will be overwritten.')
+nx.write_graphml_xml(G, "results/xml")
+nx.write_adjlist(G, 'results/adjlist')
+
+
+
