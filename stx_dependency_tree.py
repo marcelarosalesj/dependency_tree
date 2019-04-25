6682c73d3d90833a79f25b2526b12d426ca6020a
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-25 11:16:59 -0500

    Fix many issues with the graph generation
    - Each Package and BuildRequires are added to the Graph as Nodes.
    - If the Package is mistakenly added as a BR Node first, with the
    wrong spec path and project, it's updated.
    - Each Package is linked to the BR associated with the Specfile and
    the extra BR it has. This case is printed just to give extra info.
    - Results are updated with this new graph generator.
    
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- stx_dependency_tree.py
+++ stx_dependency_tree.py
@@ -8,23 +8,39 @@ import ipdb
 specfiles = glob.glob('./**/*.spec', recursive=True)
 
 G = nx.DiGraph()
-count=0
 for specfile in specfiles:
-    # Create RPM Spec Object
     sp = Spec.from_file(specfile)
-    # Get from path useful information
     stx_project = specfile.split('/stx/')[-1].split('/')[0]
-    # Parse each of the packages in a spec file
     for pkg in sp.packages:
-        G.add_node(pkg.name, path=specfile, project=stx_project)
-        for br in pkg.build_requires:
-            G.add_node(br, path=specfile, project=stx_project)
-            G.add_edge(pkg.name, br)
-        if sp.name == pkg.name:
-            count = count +1
-            for br in sp.build_requires:
-                G.add_node(br, path=specfile, project=stx_project)
-                G.add_edge(sp.name, br)
+        # Create Package Node
+        if not G.has_node(pkg.name):
+            """
+            For this logic I'm assuming a package is generated only by one
+            specfile.
+            Ex: it won't be a case where stx-one has a spec where fm.deb
+            is generated and stx-two has another spec generating fm.deb.
+            """
+            G.add_node(pkg.name, path=specfile, project=stx_project)
+        else:
+            """
+            This case is when the node was previously created by another spec
+            because it's a BuildRequires. Let's update its attributes.
+            """
+            G.node[pkg.name]['path'] = specfile
+            G.node[pkg.name]['project'] = stx_project
+
+        """
+        Create BuildRequires Nodes and link them to the Package Node.
+        Each Package Node must be linked to the Spec's BuildRequires
+        and to it's own BuildRequires (if any).
+        """
+        if len(pkg.build_requires):
+            print("Package with extra BRs: {} in {}".format(pkg.name, specfile))
+
+        build_requires = sp.build_requires + pkg.build_requires
+        for br in build_requires:
+            G.add_node(br.name, path=specfile, project=stx_project)
+            G.add_edge(pkg.name, br.name)
 
 # Generating results
 try:
@@ -33,8 +49,5 @@ except FileExistsError as e:
     print('W: results directory already exists. Files will be overwritten.')
 nx.write_graphml_xml(G, "results/xml")
 nx.write_adjlist(G, 'results/adjlist')
-
-
-print(count)
 
 
